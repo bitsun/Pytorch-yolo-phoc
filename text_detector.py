39fa99e59cdc9e9546bf18536c6940bf7cb4251c
bitsun <liubin1983@gmail.com>
2022-12-16 10:52:29 +0100

    delete pyc file and have fix for python 3.9 and pytorch 1.11

--- text_detector.py
+++ text_detector.py
@@ -1,9 +1,9 @@
-import cPickle
+#import cPickle
 from utils import *
 from darknet import Darknet
 import os
 from tqdm import tqdm
-
+import cv2
 # Detect if there is text present in an image and store it
 
 def detect(cfgfile, weightfile, imgfolder, text_file):
@@ -40,19 +40,28 @@ def detect(cfgfile, weightfile, imgfolder, text_file):
 
             sized = img.resize((m.width, m.height))
 
-            conf_threshold = 0.9
+            conf_threshold = 0.3
             # ORIGINAL
-            nms_threshold = 0.8
+            nms_threshold = 0.7
 
             # TEST  with better results
             # nms_threshold = 0.2
-
+            img = cv2.imread(img_full_path)
             for i in range(1):
                 start = time.time()
                 boxes = do_detect(m, sized, conf_threshold, nms_threshold, use_cuda)
                 finish = time.time()
                 #print('%s: Predicted in %f seconds.' % (imgfile, (finish - start)))
-
+                for box in boxes:
+                    x = box[0].item()
+                    y = box[1].item()
+                    w = box[2].item()
+                    h = box[3].item()
+                    x = x - w/2
+                    y = y - h/2
+                    cv2.rectangle(img, (int(img.shape[1]*x),int(img.shape[0]*y)),  (int(img.shape[1]*(x+w)),int(img.shape[0]*(y+h))),(255, 0, 255) , 2)
+                cv2.imshow("test", img) 
+                cv2.waitKey(0)
             # RESULT PATH
             #result_image_path = destination_folder + imgfile
 
@@ -70,12 +79,17 @@ def detect(cfgfile, weightfile, imgfolder, text_file):
 
 
 
-
+import torch
+import torch.nn as nn
 if __name__ == '__main__':
-
-    imgfolder = '/SSD/new_Product_Dataset/final_images/'
-    text_file = '/SSD/new_Product_Dataset/Images_with_Text.txt'
-
+    imgfolder = "C:\\Users\\bliu\\Pictures\\test\\"
+    text_file = "C:\\Users\\bliu\\Pictures\\testImages_with_Text.txt"
+    model = torch.nn.Sequential()
+    model.add_module('conv{0}'.format(1), nn.Conv2d(3, 32, 3, 2, 1, bias=False))
+    model.add_module('bn{0}'.format(1), nn.BatchNorm2d(32))
+    model.add_module('leaky{0}'.format(1), nn.LeakyReLU(0.1, inplace=True))
+    test = torch.rand(1,3,608,608)
+    model(test)
     #if not os.path.exists(destination_folder):
     #   os.mkdir(destination_folder)
 
